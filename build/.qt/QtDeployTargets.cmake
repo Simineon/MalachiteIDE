2e68b3df8033184d3d76081a06cde0744b55f96b
Simineon <tyulenev11@internet.ru>
2025-11-15 00:11:36 +0500

    Remove ignored files

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Malachite_FILE /Users/semiontyulenev/Desktop/My Folder/C++/Malachite/build/Malachite)
-set(__QT_DEPLOY_TARGET_Malachite_TYPE EXECUTABLE)
--- build/CMakeFiles/simple_window.dir/simple_window_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/simple_window.dir/simple_window_autogen/mocs_compilation.cpp.o: \
-  /Users/semiontyulenev/Desktop/My\ Folder/C++/Malachite/build/simple_window_autogen/mocs_compilation.cpp
